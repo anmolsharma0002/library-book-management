cab42f204d047c98da90de409c0350360d4c0234
Anmol Sharma <anmol.sds10@yahoo.com>
2024-09-10 02:57:26 +0530

    Setup Helpers

---
+++ helpers/jwt.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+const { tokenIssuer, tokenSecret } = require('../config/env');
+
+
+/**
+ * Generates a JSON Web Token (JWT) for a user.
+ * 
+ * @param {ObjectId} userId - The _id of the user.
+ * @param {string} userRole - The role of the user.
+ * @returns {Promise<string>} A promise that resolves to the generated JWT.
+ */
+
+const signUserAccessToken = (userId, userRole) => {
+    const payload = { app: 'client', role: userRole, id: userId };
+
+    const options = { 
+        expiresIn: '7d', 
+        issuer:tokenIssuer, 
+        audience: `${userId}`  // "audience" either it should be a string or array 
+    };
+  
+    return new Promise((resolve, reject) => {
+      jwt.sign(payload, tokenSecret, options, (err, token) => {
+        if (err) {
+          console.error('[ERROR:signUserAccessToken]', err);
+          reject(createError.InternalServerError());
+        } else {
+          resolve(token);
+        }
+      });
+    });
+};
+  
+module.exports = { 
+    signUserAccessToken 
+};
